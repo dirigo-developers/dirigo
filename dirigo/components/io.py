5296e661ae4eaea069f5fec9459fd66a3db6d742
Timothy Weber <tweber@mit.edu>
2025-01-03 15:55:31 -0500

    cleaned io; added extra dependency

--- dirigo/components/io.py
+++ dirigo/components/io.py
@@ -1,9 +1,7 @@
-from dataclasses import dataclass, fields, MISSING
+from dataclasses import dataclass
 from pathlib import Path
 import tomllib
 
-from pint import UnitRegistry, UndefinedUnitError, DimensionalityError
-import numpy as np
 
 
 
@@ -18,181 +16,8 @@ def load_toml(file_name:Path|str) -> dict:
     return toml_contents
 
 
-ureg = UnitRegistry(autoconvert_offset_to_baseunit=True) # allows use of logarithm units such as dB
-def to_si_units(quantity_with_units:str):
-    """ 
-    Converts quantity with units, represented in a string, to SI base units. 
-    """
-    q = ureg.Quantity(quantity_with_units)
-    return q.to_base_units().magnitude
-
-
-def parse_quantity(value: str):
-    """Attempts to parse a string with units into a Quantity."""
-    try:
-        print(value)
-        quantity = ureg.Quantity(value)  # Try to parse the string
-        print(quantity)
-        return quantity
-    except (TypeError, ValueError, UndefinedUnitError, DimensionalityError) as e:
-        return None
-
-
-class UnitAwareDataclass:
-    @classmethod
-    def parse_from_dict(cls, data: dict) -> 'UnitAwareDataclass':
-        init_args = {} # dict that will generate the dataclass after parsing
-        for f in fields(cls):
-            value = data.get(f.name, MISSING)
-            if value is not MISSING:
-
-                if type(value) is not list:
-                    # if this is not a list, then try parsing
-                    q = parse_quantity(value)
-
-                    if q is not None:
-                        init_args[f.name] = to_si_units(value)
-                    else:
-                        init_args[f.name] = value
-
-                else:
-                    # if this is a list, try parsing each element
-                    for i,element in enumerate(value):
-                        e = parse_quantity(element)
-
-                        if e is not None:
-                            value[i] = to_si_units(element)
-
-                    init_args[f.name] = value
-
-        return cls(**init_args)   
-
-
-@dataclass
-class LoggerConfig: # no units
-    type: str
-    save_path: str
-
-
-@dataclass
-class LaserConfig(UnitAwareDataclass):
-    pulsed: bool
-    pulse_frequency: float
-
-
-@dataclass
-class ScannerRange(UnitAwareDataclass): # REMOVE?
-    voltage_fast: list[float]
-    voltage_slow: list[float]
-    angle_fast: float
-    angle_slow: float
-
-    @property
-    def voltage_fast_min(self):
-        return self.voltage_fast[0]
-    
-    @property
-    def voltage_fast_max(self):
-        return self.voltage_fast[1]
-    
-    @property
-    def voltage_slow_min(self):
-        return self.voltage_slow[0]
-    
-    @property
-    def voltage_slow_max(self):
-        return self.voltage_slow[1]
-    
-    @property
-    def volts_per_optical_degree_fast(self):
-        return self.voltage_fast_max / self.angle_fast
-    
-    @property
-    def volts_per_optical_degree_slow(self):
-        vrange = self.voltage_slow_max - self.voltage_slow_min
-        return vrange / self.angle_slow
-
-
-@dataclass
-class ScannerOptics(UnitAwareDataclass): # REMOVE?
-    relay_mag: int
-    objective_fl: float
-
-    def angle_to_position(self, scan_angle):
-        """ Converts scan angle to position """
-        angle_at_objective = scan_angle / self.relay_mag
-        position = np.sin(angle_at_objective) * self.objective_fl
-        return position
-
-
-@dataclass
-class ScannerWiring: # no units # REMOVE
-    fast_scanner_signal_out: str
-    slow_scanner_signal_out: str
-    fast_scanner_sync_in: str
-    frame_clock_out: str
-    frame_clock_in: str
-
-
-@dataclass
-class ScannerConfig(UnitAwareDataclass): # REMOVE
-    fast_axis: str
-    nominal_scanner_frequency: float
-    flip_fast: bool
-    flip_slow: bool
-    scan_range: ScannerRange
-    optics: ScannerOptics 
-    wiring: ScannerWiring 
-    daq_sample_rate: float
-    fast_scanner_settle_time: float
-    slow_scanner_response_time: float
-
-    def _voltage_to_scan_width(self, voltage):
-        vpod = self.scan_range.volts_per_optical_degree_fast
-        angle = voltage / vpod
-        return 2*self.optics.angle_to_position(angle/2)
-
-    @property
-    def fast_width_max(self):
-        voltage = self.scan_range.voltage_fast_max
-        return self._voltage_to_scan_width(self, voltage)
-    
-    @property
-    def fast_width_min(self):
-        voltage = self.scan_range.voltage_fast_min
-        return self._voltage_to_scan_width(self, voltage)
-
-
-@dataclass
-class DigitizerConfig(UnitAwareDataclass):
-    manufacturer: str
-    model: str
-    nbuffers: int
-    clock_source: str
-    sample_rate: float
-    clock_edge: str
-    input_coupling: list[str]
-    input_range: list[float]
-    input_impedance: list[int]
-    trigger_source: str
-    trigger_slope: str
-    trigger_level: float
-    external_trigger_coupling: str
-    external_trigger_range: str
-    aux_io_mode: str
-    aux_io_parameter: int
-
-
-@dataclass
-class StageConfig(UnitAwareDataclass):
-    manufacturer: str
-    model: str
-
-
 @dataclass
 class SystemConfig:
-    logger: LoggerConfig
-    laser: LaserConfig
     optics: dict
     digitizer: dict
     stage: dict
@@ -203,8 +28,6 @@ class SystemConfig:
     def from_toml(cls, toml_path: Path) -> 'SystemConfig':
         data = load_toml(toml_path)
         return cls(
-            logger=LoggerConfig(**data.get("logger", {})),
-            laser=LaserConfig.parse_from_dict(data["laser"]),
             optics=data['optics'],
             digitizer=data["digitizer"],
             stage=data["stage"],
