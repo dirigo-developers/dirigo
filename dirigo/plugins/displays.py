added rolling frame averaging and fixed issue with dewarping

dirigo/plugins/displays.py
@@ -1,4 +1,5 @@
 import time
+import threading
 
 import numpy as np
 from numba import njit, prange, types
@@ -6,6 +7,27 @@ from numba import njit, prange, types
 from dirigo.sw_interfaces import Display, Acquisition, Processor
 from dirigo.sw_interfaces.display import ColorVector, DisplayChannel
 
+
+
+@njit(
+    (types.uint16[:,:,:,:], types.int64, types.uint16[:,:,:]),
+    nogil=True, parallel=True, fastmath=True, cache=True
+)
+def rolling_average_kernel(ring_buffer: np.ndarray, frame_index: int, averaged: np.ndarray) -> np.ndarray:
+    Nf, Ny, Nx, Nc = ring_buffer.shape
+    d = min(Nf, frame_index + 1)
+
+    for iy in prange(Ny):
+        for ix in prange(Nx):
+            for ic in range(Nc):
+
+                tmp = 0
+                for iframe in range(Nf):
+                    tmp += ring_buffer[iframe, iy, ix, ic]
+
+                averaged[iy, ix, ic] = tmp // d
+
+    return averaged
 
 
 # Generate gamma correction LUT
@@ -61,10 +83,14 @@ class FrameDisplay(Display):
     def __init__(self, acq: Acquisition, proc: Processor):
         super().__init__(acq, proc)
 
-        data_range = acq.hw.data_range if acq else proc._acq.hw.data_range 
+        self._n_frame_average = 8
 
-        self._prev_data = None # Indicates that no data has been acquired yet
+        self._prev_data = None # None indicates that no data has been acquired yet
+        self._average_buffer = None
+        self._i: int = 0 # tracks rolling average index
+        self._average_buffer_lock = threading.Lock()  # Add a lock for thread safety
 
+        data_range = acq.hw.data_range if acq else proc._acq.hw.data_range
         self.luts = np.zeros(shape=(self.nchannels, data_range.range, 3), dtype=np.uint16)
 
         self.display_channels: list[DisplayChannel] = []
@@ -79,10 +105,6 @@ class FrameDisplay(Display):
             )
             self.display_channels.append(dc)
 
-        # TODO: something with these attributes
-        self.blending_mode = 'additive'
-        self.gamma = 1.0 # 2.2 is common
-
 
     def run(self):
         while True:
@@ -96,16 +118,39 @@ class FrameDisplay(Display):
                 # the appearance of the last acquire frame)
                 return 
             
-            self._prev_data = data # store reference for use after thread finishes
             t0 = time.perf_counter()
-            if self.blending_mode == 'additive':
-                processed = additive_display_kernel(data, self.luts)
-            # elif self.blending_mode == 'subtractive':
-            #     processed = subtractive_display_kernel(data, self.luts)
-            t1 = time.perf_counter()
-            #print(f"Channel blending: {1000*(t1-t0):.1f}ms")
+            with self._average_buffer_lock:
+                # _average_buffer is initially None and the first frame establishes height, width, etc
+                # Averaging can also be reset or changed by setting this to None, prompting reallocation
+                if self._average_buffer is None:
+                    buffer_shape = (self.n_frame_average,) + data.shape
+                    self._average_buffer = np.zeros(buffer_shape, dtype=np.uint16)
+                    averaged_frame = np.zeros(data.shape, np.uint16)
+                    self._i = 0
 
-            self.publish(processed)
+                self._average_buffer[self._i % self.n_frame_average] = data
+
+                rolling_average_kernel(self._average_buffer, self._i, averaged_frame)
+                processed = additive_display_kernel(averaged_frame, self.luts)
+
+                self.publish(processed)
+                self._prev_data = averaged_frame # store reference for use after thread finishes  
+
+            t1 = time.perf_counter()
+            self._i += 1
+            print(f"Channel display processing: {1000*(t1-t0):.1f}ms")
+                     
+    @property
+    def n_frame_average(self) -> int:
+        return self._n_frame_average
+    
+    @n_frame_average.setter
+    def n_frame_average(self, n_frames: int):
+        if not isinstance(n_frames, int) or n_frames < 1:
+            raise ValueError("Rolling frame average must be an integer >= 1.")
+        with self._average_buffer_lock:
+            self._n_frame_average = n_frames
+            self._average_buffer = None # Triggers reset of the average buffer 
 
     def update_display(self, skip_when_acquisition_in_progress: bool = True):
         """On demand reprocessing of the last acquired frame for display.

dirigo/plugins/processors.py
@@ -32,7 +32,7 @@ def dewarp_kernel(buffer_data: np.ndarray, dewarped: np.ndarray,
     Ndirections = start_indices.shape[0]
     
     for si in prange(Ns): # si = Slow pixel Index
-        fwd_rvs = si % Ndirections # denotes whether this line is 'forward' (0) or 'reverse' (1) scan (unidirectional scanning is only 'forward')
+        fwd_rvs = si % Ndirections # denotes 'forward' (0) or 'reverse' (1) scan (unidirectional scanning is only 'forward')
         ri = si // Ndirections # record index (for unidirectional scanning ri=si)
         stride = 1 - 2 * fwd_rvs # for stride in range later, 0 -> 1 and 1 -> -1 
 
@@ -41,7 +41,9 @@ def dewarp_kernel(buffer_data: np.ndarray, dewarped: np.ndarray,
             start = start_indices[fwd_rvs, fi]
 
             #Requesting indices outside sampled data: skip
-            if (start < 0) or (start > Nsamples): 
+            if (start < 0) or (start > Nsamples):
+                dewarped[si, fi, 0] = 0
+                dewarped[si, fi, 1] = 0
                 continue
 
             tmp0 = types.int32(0)
@@ -58,21 +60,34 @@ def dewarp_kernel(buffer_data: np.ndarray, dewarped: np.ndarray,
 
 
 @njit(
-    types.float32[:,:](types.uint16[:,:,:]), 
+    types.float32[:,:](types.uint16[:,:,:], types.int64, types.int64, types.float64), 
     nogil=True, parallel=True, fastmath=True, cache=True
 )
-def window_bidi_data(data: np.ndarray):
-    """Select ranages and convert to float32."""
+def window_bidi_data(data: np.ndarray, lines_per_frame: int, trigger_delay:int, samples_per_period: float):
+    """Select ranages and convert to float32.
+
+    Note: operates on channel 0 only.
+    """
     Nrec, Ns, Nc = data.shape # records, samples, channels
-    mid = Ns // 2 # mid point in the record
-    r = 2**int(np.log2(Ns)) // 2 # half window
+
+    # Largest power of two window
+    n = 2**int(np.log2(Ns / 2 - trigger_delay))
+
+    # Find the nominal midpoints and endpoints of fwd and rvs scans
+    mid_fwd = samples_per_period/4
+    mid_rvs = mid_fwd + samples_per_period/2
+
+    fwd0 = int(mid_fwd) - n//2 - trigger_delay
+    fwd1 = int(mid_fwd) + n//2 - trigger_delay
+    rvs0 = int(mid_rvs) - n//2 - trigger_delay
+    rvs1 = int(mid_rvs) + n//2 - trigger_delay
 
     # Make fp32 output array
-    out_array = np.empty((2*Nrec, r), dtype=np.float32)
+    out_array = np.zeros((lines_per_frame, n), dtype=np.float32)
 
-    for rec in prange(Nrec):
-        out_array[2*rec, :] = data[rec, (mid-r):mid, 0].astype(np.float32)
-        out_array[2*rec+1, ::] = data[rec, (mid+r):mid:-1, 0].astype(np.float32)
+    for rec in prange(lines_per_frame//2):
+        out_array[2*rec, :] = data[rec, fwd0:fwd1, 0].astype(np.float32)
+        out_array[2*rec+1, :] = data[rec, rvs1:rvs0:-1, 0].astype(np.float32) # flipped with the slicing
 
     return out_array
         
@@ -85,7 +100,7 @@ def compute_cross_power_spectrum(F: np.ndarray):
     """Compute the cross-power spectrum for F."""
     n_freqs = F.shape[1]  # Number of frequency components
     n_pairs = F.shape[0] // 2  # Number of record pairs
-    xps = np.empty(n_freqs, dtype=np.complex64)  # Output array
+    xps = np.zeros(n_freqs, dtype=np.complex64)  # Output array
 
     for i in prange(n_freqs * n_pairs):
         pair = i // n_freqs
@@ -93,8 +108,10 @@ def compute_cross_power_spectrum(F: np.ndarray):
         xps[freq] += F[2 * pair, freq] * np.conj(F[2 * pair + 1, freq])  
 
     # normalize
+    EPS = 1e-1  # Small constant
     for freq in prange(n_freqs):
-        xps[freq] /= np.abs(xps[freq])
+        mag = np.abs(xps[freq]) + EPS
+        xps[freq] /= mag
 
     return xps
 
@@ -112,12 +129,13 @@ class RasterFrameProcessor(Processor):
         # Initialize with the nominal rate, will measure with data timestamps
         self._fast_scanner_frequency = self._acq.hw.fast_raster_scanner.frequency
 
+        # Pre-allocate array for dewarped image
         self.dewarped = np.zeros(self.dewarped_shape, dtype=np.uint16)
         
     def run(self):
         trigger_delay = self._acq.hw.digitizer.acquire.trigger_delay_samples # not adjustable during acquisition
-        trigger_phase = units.Angle(0.0)
-        start_indices = self.calculate_start_indices() - trigger_delay
+        trigger_phase = 0
+        start_indices = self.calculate_start_indices(trigger_phase) - trigger_delay
         nsamples_to_sum = np.abs(np.diff(start_indices, axis=1))
         
         while True: # Loops until receives sentinel None
@@ -128,31 +146,31 @@ class RasterFrameProcessor(Processor):
                 print('Exiting processing thread')
                 return # concludes run() - this thread ends
 
-            if buf.positions is not None:
-                print(f'processor got positions: {buf.positions[0]}')
+            # if buf.positions is not None:
+            #     print(f'processor got positions: {buf.positions[0]}')
 
             t0 = time.perf_counter()
             dewarp_kernel(buf.data, self.dewarped, start_indices, nsamples_to_sum)
-            t1 = time.perf_counter()
             self.publish(self.dewarped) # sends off to Logger or Display workers
-
-            print(f"{self.native_id} [max sum: {np.max(nsamples_to_sum)}] Processed a frame in {1000*(t1-t0):.02f} ms")
+            t1 = time.perf_counter()
+            
+            #print(f"{self.native_id} [max sum: {np.max(nsamples_to_sum)}] Processed a frame in {1000*(t1-t0):.02f} ms")
 
             # If timestamps are assigned (default is None)
             if isinstance(buf.timestamps, np.ndarray):
                 # Measure frequency from timestamps
                 self._fast_scanner_frequency = 1 / np.mean(np.diff(buf.timestamps))
 
-                # Update dewarping start indices
-                start_indices = self.calculate_start_indices(trigger_phase) - trigger_delay
-                nsamples_to_sum = np.abs(np.diff(start_indices, axis=1))
-
             # Measure phase from bidi data (in uni-directional, phase is not critical)
-            # if self._spec.bidirectional_scanning:
-            #     trigger_phase = self.measure_phase(buf.data)
+            if self._spec.bidirectional_scanning:
+                trigger_phase = self.measure_phase(buf.data)
+
+            # Update dewarping start indices
+            start_indices = self.calculate_start_indices(trigger_phase) - trigger_delay
+            nsamples_to_sum = np.abs(np.diff(start_indices, axis=1))
 
 
-    def calculate_start_indices(self, trigger_phase: units.Angle = units.Angle(0.0)):
+    def calculate_start_indices(self, trigger_phase: int = 0):
         # Set up an array with the SPATIAL edges of pixels--we will bin samples into the pixel edges
         ff = self._spec.fill_fraction
         pixel_edges = np.linspace(ff, -ff, self._spec.pixels_per_line + 1)
@@ -160,7 +178,7 @@ class RasterFrameProcessor(Processor):
         # Create a dewarping function based on fast axis waveform type
         waveform = self._acq.hw.fast_raster_scanner.waveform
         if waveform == "sinusoid":
-            # arccos inverts the cosinusoidal path, normalize to 0.0 to 1.0
+            # arccos inverts the cosinusoidal path, normalize scan period to 0.0 to 1.0
             temporal_edges = np.arccos(pixel_edges) / TWO_PI
         elif waveform == "sawtooth":
             pass # like a polygon scanner
@@ -176,35 +194,42 @@ class RasterFrameProcessor(Processor):
         else:
             temporal_edges = np.vstack([temporal_edges])
 
-        p = self._acq.hw.digitizer.sample_clock.rate / self._fast_scanner_frequency 
+        # TODO compute only once: Up to here is completely the same each iteration 
         
-        starts_exact = (temporal_edges - (trigger_phase / TWO_PI)) * p 
-        start_indices = np.ceil(starts_exact).astype(np.int32)
+        starts_exact = temporal_edges * self.samples_per_period + trigger_phase
+
+        start_indices = np.ceil(starts_exact).astype(np.int32) # Forward scan should be ceil, Reverse scan should be floor
 
         return start_indices
     
     def measure_phase(self, data: np.ndarray) -> units.Angle:
         """Measure the apparent fast raster scanner trigger phase for bidirectional acquisitions."""
-        UPSAMPLE = 4
+        UPSAMPLE = 8
+        
+        data_window = window_bidi_data(data, self._spec.lines_per_frame, self._acq.hw.digitizer.acquire.trigger_delay_samples, self.samples_per_period) # todo preallocate data_window
 
-        data_window = window_bidi_data(data)
         F = fft.rfft(data_window, axis=1, workers=4)
         xps = compute_cross_power_spectrum(F)
 
         n = data_window.shape[1] * UPSAMPLE
         corr = np.abs(fft.irfft(xps, n))
+
         shift = np.argmax(corr)
-        if shift > n // 2:  # Handle wrap-around for negative shifts
+        print("N", n, "SHIFT", shift)
+        if shift > (n//2):  # Handle wrap-around for negative shifts
             shift -= n
 
-        phase = units.Angle(
-            TWO_PI * (shift / UPSAMPLE) * self._fast_scanner_frequency / self._sample_clock_rate
-        )
+        print(f"Estimated shift (samples): {shift / UPSAMPLE}")
 
-        print(f"Estimated shift: {phase}")
-
-        return phase
+        #return phase
+        return shift / UPSAMPLE / 2
     
     @cached_property
     def _sample_clock_rate(self) -> units.SampleRate:
         return self._acq.hw.digitizer.sample_clock.rate
+    
+    @property
+    def samples_per_period(self) -> float:
+        """The exact number of digitizer samples per fast raster scanner period.
+        """
+        return self._sample_clock_rate / self._fast_scanner_frequency 