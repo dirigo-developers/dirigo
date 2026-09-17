aa7e619c1f06681b6d7819fbdef8b53a80f02ba9
Tim Weber <tweber@mit.edu>
2026-09-17 10:38:03 -0400

    added a line camera raw data Loader

--- dirigo/plugins/loaders.py
+++ dirigo/plugins/loaders.py
@@ -11,7 +11,7 @@ from dirigo.plugins.writers import (
     read_digitizer_profile, read_positions, read_timestamps,
     read_sequence_indices, read_strip_indices, read_depth_indices, read_volume_indices
 )
-from dirigo.plugins.acquisitions import FrameAcquisitionSpec
+from dirigo.plugins.acquisitions import FrameAcquisitionSpec, LineCameraAcquisitionSpec
 
 
 
@@ -36,10 +36,12 @@ def deserialize_float64_list(blob: bytes):
 
 
 class RawRasterFrameLoader(Loader):   
-    def __init__(self, 
-                 file_path: str | Path, 
-                 spec_class: Type[FrameAcquisitionSpec] = FrameAcquisitionSpec):
-        super().__init__(file_path, thread_name="Frame loader")
+    def __init__(
+        self, 
+        file_path: str | Path, 
+        spec_class: Type[FrameAcquisitionSpec] = FrameAcquisitionSpec
+    ):
+        super().__init__(file_path, thread_name="Raster Frame loader")
 
         with tifffile.TiffFile(self._file_path) as tif:   
             page = tif.pages[0]
@@ -63,6 +65,64 @@ class RawRasterFrameLoader(Loader):
         try:
             with tifffile.TiffFile(self._file_path) as tif:   
                 
+                while self.frames_read < len(tif.pages):
+                    frame = self._get_free_product()
+
+                    # Copy raw data
+                    frame.data[...] = tif.pages[self.frames_read].asarray()
+
+                    # Copy metadata, if available
+                    if self._timestamps:
+                        frame.timestamps = self._timestamps[self.frames_read]
+                    if self._positions:
+                        frame.positions = self._positions[self.frames_read]
+                    if self._sequence_indices:
+                        frame.sequence_index = self._sequence_indices[self.frames_read]
+                    if self._strip_indices:
+                        frame.strip_index = self._strip_indices[self.frames_read]
+                    if self._depth_indices:
+                        frame.depth_index = self._depth_indices[self.frames_read]
+                    if self._volume_indices:
+                        frame.volume_index = self._volume_indices[self.frames_read]
+
+                    print(f"publishing frame {self.frames_read}")
+                    self._publish(frame)
+
+                    self.frames_read += 1
+        finally:
+            self._publish(None) # sentinel coding finished
+
+
+class RawCameraFrameLoader(Loader):
+    def __init__(
+        self, 
+        file_path: str | Path, 
+        spec_class: Type[LineCameraAcquisitionSpec] = LineCameraAcquisitionSpec
+    ):
+        super().__init__(file_path, thread_name="Camera Frame loader")
+
+        with tifffile.TiffFile(self._file_path) as tif:   
+            page = tif.pages[0]
+            self._init_product_pool(n=4, shape=page.shape, dtype=page.dtype)
+
+        self.system_config = read_system_config(self._file_path)
+        self.spec = spec_class(**read_acquisition_spec(self._file_path))
+        self.runtime_info = read_runtime_info(self._file_path)
+        # self.camera_profile = read_camera_profile(self._file_path)
+
+        self._timestamps = None
+        self._positions = read_positions(self._file_path)
+        self._sequence_indices = read_sequence_indices(self._file_path)
+        self._strip_indices = read_strip_indices(self._file_path)
+        self._depth_indices = read_depth_indices(self._file_path)
+        self._volume_indices = read_volume_indices(self._file_path)
+
+        self.frames_read = 0
+
+    def _work(self):
+        try:
+            with tifffile.TiffFile(self._file_path) as tif:   
+
                 while self.frames_read < len(tif.pages):
                     frame = self._get_free_product()
 
